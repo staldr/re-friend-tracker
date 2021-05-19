c40dbec853ba2c70b58686f88147ecf323c6497d
staldr <stlr@zhaw.ch>
2021-05-19 12:44:51 +0000

    v5

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "STLR Friends",
+            "title": "STLR My Friends",
             "formList": [
                 {
                     "id": "FriendForm",
